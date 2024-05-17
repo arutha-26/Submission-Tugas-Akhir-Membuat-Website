24398d004aebda84f7dcab3ec77a68f8eb5adaa2
Arutha <arthafriday@gmail.com>
2024-05-17 22:30:27 +0800

    sticky navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,17 @@ let currentIndex = 0;
 function showSlide(index) {
     const carouselWrapper = document.querySelector('.carousel-wrapper');
     const slides = document.querySelectorAll('.card');
-    const totalSlides = slides.length;
+    const totalSlides = slides.length - 1;
 
     if (index >= totalSlides) {
-        currentIndex = 0;
+        currentIndex = 0; 
     } else if (index < 0) {
-        currentIndex = totalSlides - 1;
+        currentIndex = totalSlides - 1; 
     } else {
         currentIndex = index;
     }
 
-    const offset = -currentIndex * 320; // Adjust according to card width + margin
+    const offset = -currentIndex * 320; 
     carouselWrapper.style.transform = `translateX(${offset}px)`;
 }
 
@@ -25,5 +25,18 @@ function prevSlide() {
     showSlide(currentIndex - 1);
 }
 
-// Initialize the carousel
 showSlide(currentIndex);
+
+
+// sticky javascript
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const sticky = navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
+
